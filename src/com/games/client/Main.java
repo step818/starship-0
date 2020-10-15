c8926d8869421200133319233db2a940459ec376
AndrewSzymanek <andrew.szymanek@comcast.net>
2020-10-15 17:26:15 -0500

    turned game class into singleton. added getters/setters for private static variables

--- src/com/games/client/Main.java
+++ src/com/games/client/Main.java
@@ -12,9 +12,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
 //        start the game here
         System.out.println("Welcome to Starship");
-        Game game = new Game();
-        // for game board, call the game.begin() below
-        game.begin(80,24);
+        Game.getGameInstance().begin(80,24);
 //        Earth.draw();
 //        Moon.draw();
 
